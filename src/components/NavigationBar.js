60c05e4a4789e9a74e41aff1da1c64ea55910a4a
Faisal Younus <gitfyhub>
2020-02-01 17:41:57 -0600

    Fix error with Warning: validateDOMNesting(...) in src/components/NavigationBar.js

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
 
 const Styles = styled.div`
   .navbar {
     background-color: #222;
   }
 
-  a, .navbar-brand, .navbar-nav .nav-link {
+  a,
+  .navbar-brand,
+  .navbar-nav .nav-link {
     color: #bbb;
 
     &:hover {
@@ -21,26 +23,20 @@ export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
       <Navbar.Brand href="/">Code Life</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle arial-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
+            <Link to="/">Home</Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/about">About</Link>
-            </Nav.Link>
+            <Link to="/about">About</Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link>
-              <Link to="/contact">Contact</Link>
-            </Nav.Link>
+            <Link to="/contact">Contact</Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  </Styles >
-)
+  </Styles>
+);
